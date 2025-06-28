783196d932ab5b61107a0d554c255ef72703a14b
ISAMEMAIO <joooommm@outlook.com>
2025-06-28 09:45:54 -0700

    Update i.js

--- static/assets/js/i.js
+++ static/assets/js/i.js
@@ -8,58 +8,29 @@ const form = document.getElementById("fv");
 const input = document.getElementById("iv");
 
 if (form && input) {
-  form.addEventListener("submit", async event => {
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
-    if (window.top.location.pathname === "/rx") {
-      processUrl(input.value, "");
-    } else {
-      processUrl(input.value, "/rx");
-    }
+    processUrl(input.value);
   });
 }
 
-function processUrl(value, path) {
+function processUrl(value) {
   let url = value.trim();
-  const engine = localStorage.getItem("engine");
-  // Set Bing as default if no engine set
-  const searchUrl = engine ? engine : "https://www.bing.com/search?q=";
-
+  const engine = localStorage.getItem("engine") || "https://www.bing.com/search?q=";
   if (!isUrl(url)) {
-    url = searchUrl + url;
+    url = engine + encodeURIComponent(url);
   } else if (!(url.startsWith("https://") || url.startsWith("http://"))) {
     url = `https://${url}`;
   }
-
   sessionStorage.setItem("GoUrl", __uv$config.encodeUrl(url));
   const dy = localStorage.getItem("dy");
-
   if (dy === "true") {
     window.location.href = `/a/q/${__uv$config.encodeUrl(url)}`;
-  } else if (path) {
-    location.href = path;
   } else {
     window.location.href = `/a/${__uv$config.encodeUrl(url)}`;
   }
 }
 
-function go(value) {
-  processUrl(value, "/rx");
-}
-
-function blank(value) {
-  processUrl(value);
-}
-
-function dy(value) {
-  processUrl(value, `/a/q/${__uv$config.encodeUrl(value)}`);
-}
-
 function isUrl(val = "") {
-  if (
-    /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
-  ) {
-    return true;
-  }
-  return false;
+  return /^http(s?):\/\//.test(val) || (val.includes(".") && val.substr(0, 1) !== " ");
 }
